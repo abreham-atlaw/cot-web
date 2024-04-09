4354f2ee26925973de43f3ff128b9325d7c0acce
Tsega Yaekob <tsegayaekob7@gmail.com>
2024-04-09 08:59:39 +0300

    cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import "./App.css";
 import "./index.css";
-import StaffView from "./apps/staff/presentation/views/Staff";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import StaffDetail from "./apps/staff/presentation/views/StaffDetail";
-import SideBar from "./common/components/views/SideBar";
-import AssetView from "./apps/asset/presentation/views/AssetView";
-import AssetDetail from "./apps/asset/presentation/views/AssetDetail";
+// import StaffView from "./apps/staff/presentation/views/Staff";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import StaffDetail from "./apps/staff/presentation/views/StaffDetail";
+// import SideBar from "./common/components/views/SideBar";
+// import AssetView from "./apps/asset/presentation/views/AssetView";
+// import AssetDetail from "./apps/asset/presentation/views/AssetDetail";
+import DashBoard from "./apps/admin/presentation/views/Dashboard";
 
 function App() {
 	return (
 		<div className='app'>
-			<SideBar />
+			<DashBoard/>
+			{/* <SideBar />
 			<div className='main-content'>
 				<BrowserRouter>
 					<Routes>
@@ -20,7 +22,7 @@ function App() {
 						<Route path='assets/:assetId' element={<AssetDetail />} />
 					</Routes>
 				</BrowserRouter>
-			</div>
+			</div> */}
 		</div>
 	);
 }
