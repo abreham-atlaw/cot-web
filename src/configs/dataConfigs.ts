2604eba6efb7250eb56b443eb96e2b910ab8c7e7
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-05-25 07:52:53 +0300

    Production Changes

--- src/configs/dataConfigs.ts
+++ src/configs/dataConfigs.ts
@@ -2,7 +2,7 @@ export default class DataConfigs{
 
     static readonly RPC_URL = "https://cot.alwaysdata.net";
     static readonly API_URL = "https://cot-api.alwaysdata.net/api/v1";
-    static readonly FRONTEND_URL = "http://localhost:5173";
+    static readonly FRONTEND_URL = "https://cot-et.vercel.app";
     static readonly FILE_HOST_ADDRESS = "https://drive.alwaysdata.net/";
 
     static readonly GOOGLE_RECAPTCHA_KEY = "6LeqtOQpAAAAAInLAahnluC5gy6NEkc02W5SpC9g";
