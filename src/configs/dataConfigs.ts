5f6ccb64ef56054e0981c00be3e3bc89da371298
Abreham Atlaw <abrishatlaw@gmail.com>
2024-06-08 16:10:39 +0300

    Node Migration

--- src/configs/dataConfigs.ts
+++ src/configs/dataConfigs.ts
@@ -1,6 +1,6 @@
 export default class DataConfigs{
 
-    static readonly RPC_URL = "https://api.edventures.ai:8545";
+    static readonly RPC_URL = "https://demo.edventures.ai:8545";
     static readonly API_URL = "https://api.edventures.ai:8890/api/v1";
     static readonly FRONTEND_URL = "https://cot-et.vercel.app";
     static readonly FILE_HOST_ADDRESS = "https://bareknuckle-seaman.000webhostapp.com/"
