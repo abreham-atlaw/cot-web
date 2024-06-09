20434252df5d83e20262b8a0f6b0b3d943d7c5ab
Abreham Atlaw <abrishatlaw@gmail.com>
2024-06-09 20:07:30 +0300

    Fixed authentication redirect on staff detail page.

--- src/common/viewmodel/modelDetailVIewModel.ts
+++ src/common/viewmodel/modelDetailVIewModel.ts
@@ -1,3 +1,4 @@
+import AuthRepository from "@/apps/auth/infrastructure/repositories/authRepository";
 import EtherModel from "../model/model";
 import EthersModelRepository from "../repositories/ethersModelRepository";
 import type ModelDetailState from "../state/modelDetailState";
@@ -7,6 +8,8 @@ import AsyncViewModel from "./asyncViewModel";
 export default class ModelDetailViewModel<M extends EtherModel> extends AsyncViewModel<ModelDetailState<M>>{
 
     protected repository: EthersModelRepository<M>;
+    private authRepository = new AuthRepository();
+
 
     constructor(state: ModelDetailState<M>, repository: EthersModelRepository<M>, syncState: (state: ModelDetailState<M>) => void){
         super(state, syncState);
@@ -16,6 +19,7 @@ export default class ModelDetailViewModel<M extends EtherModel> extends AsyncVie
     public async onInit(): Promise<void> {
         await super.onInit();
         this.state.instance = await this.repository.getById(this.state.id);
+        this.state.me = await this.authRepository.whoAmI();
     }
 
 
