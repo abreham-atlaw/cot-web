681bc29170aafd5c7250048bbebb529c3a55289f
Jeal Sabir <jealsabb@gmail.com>
2024-06-15 11:37:01 +0300

    Fixed build errors.

--- src/apps/auth/domain/models/invitation.ts
+++ src/apps/auth/domain/models/invitation.ts
@@ -8,14 +8,14 @@ export default class Invitation implements EtherModel {
     role: number;
     orgId: string;
     name: string;
-    createDatetime: Date; // Add this line
+    createDatetime?: Date; 
 
-    constructor(id: string | undefined, to: string, role: number, orgId: string, name: string, createDatetime: Date) {
+    constructor(id: string | undefined, to: string, role: number, orgId: string, name: string, createDatetime?: Date) {
         this.id = id;
         this.to = to;
         this.role = role;
         this.orgId = orgId;
         this.name = name;
-        this.createDatetime = createDatetime; // And this line
+        this.createDatetime = createDatetime; 
     }
 }
